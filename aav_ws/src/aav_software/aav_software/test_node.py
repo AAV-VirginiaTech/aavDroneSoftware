16906007b9abf77c4c355af6ad549b6a0549b62b
Carter Hawkins <97128719+CarterH93@users.noreply.github.com>
2026-01-10 02:09:49 +0000

    Have custom interfaces working

--- aav_ws/src/aav_software/aav_software/test_node.py
+++ aav_ws/src/aav_software/aav_software/test_node.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from ardupilot_msgs.msg import GlobalPosition
+from aav_interfaces.msg import Mode
 
 
 
@@ -11,8 +12,8 @@ class TestNode(Node):
 
     def __init__(self):
         super().__init__("test_node")
-        self.counter = 0
-        self.publisher = self.create_publisher(GlobalPosition, '/ap/cmd_gps_pose', 10)
+        self.ardupilotPublisher = self.create_publisher(GlobalPosition, '/ap/cmd_gps_pose', 10)
+        self.aavPublisher = self.create_publisher(Mode, '/aav/current_mode', 10)
         self.get_logger().info("Test Node has been launched")
         self.create_timer(1.0, self.timer_callback)
         
@@ -20,19 +21,22 @@ class TestNode(Node):
 
 
     def timer_callback(self):
-        self.get_logger().info("Test Node Count " + str(self.counter))
-        self.counter += 1
+        
+        # Use ardupilot interface
+        msg1 = GlobalPosition()
+        msg1.header.frame_id = 'map'
+        msg1.coordinate_frame = GlobalPosition.FRAME_GLOBAL_INT  # = 5
+        msg1.latitude = -35.365822
+        msg1.longitude = 149.163124
+        self.ardupilotPublisher.publish(msg1)
+        
+        # Use AAV interface
+        msg2 = Mode()
+        msg2.mode = 8
+        self.aavPublisher.publish(msg2)
         
         
-        msg = GlobalPosition()
-
-        # Match the CLI command exactly
-        msg.header.frame_id = 'map'
-        msg.coordinate_frame = GlobalPosition.FRAME_GLOBAL_INT  # = 5
-        msg.latitude = -35.365822
-        msg.longitude = 149.163124
-
-        self.publisher.publish(msg)
+        
 
 
 def main(args=None):
