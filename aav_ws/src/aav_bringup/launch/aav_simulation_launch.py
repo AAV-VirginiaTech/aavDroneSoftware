b1edddea42be34b2aeeb18c4ae48687400400ced
Carter Hawkins <97128719+CarterH93@users.noreply.github.com>
2026-02-20 03:53:38 +0000

    Updated yolo model to use

--- aav_ws/src/aav_bringup/launch/aav_simulation_launch.py
+++ aav_ws/src/aav_bringup/launch/aav_simulation_launch.py
@@ -109,7 +109,7 @@ def generate_launch_description():
             ]
         ),
         launch_arguments={
-            "model": str(Path(aav_bringup) / "aav_yolo_models" / "basic.pt"),
+            "model": str(Path(aav_bringup) / "aav_yolo_models" / "bullseye.pt"),
             "input_image_topic": "/camera/image",
         }.items(),
     )
