7115273e2b31c8372016b8de24bae4678b6fb9d5
Carter Hawkins <97128719+CarterH93@users.noreply.github.com>
2026-04-17 20:10:27 +0000

    Added all the launch files we need

--- aav_ws/src/aav_bringup/launch/aav_cuasc_package_delivery_drone_launch.py
+++ aav_ws/src/aav_bringup/launch/aav_cuasc_package_delivery_drone_launch.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 from aav_software.object_alignment_controller import Mission
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -9,6 +12,8 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     ld = LaunchDescription()
+
+    aav_bringup = get_package_share_directory("aav_bringup")
 
     # Add AAV Software nodes you want to launch here
 
@@ -24,11 +29,6 @@ def generate_launch_description():
         )
     )
 
-    location_logger = Node(
-        package="aav_software",
-        executable="location_logger",
-    )
-
     manavs_magic_code = Node(
         package="aav_software",
         executable="manavs_magic_code",
@@ -40,7 +40,6 @@ def generate_launch_description():
         parameters=[
             {"current_mission": Mission.PACKAGE_DELIVERY_CUASC.value},
             {"descent_alignment_altitude": 5.0},
-            {"hardcoded_drop_altitude": 3.0},
         ],
     )
 
@@ -49,10 +48,29 @@ def generate_launch_description():
         executable="topic_converter_for_drone",
     )
 
+    # YOLO.
+    yolo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("yolo_bringup"),
+                        "launch",
+                        "yolo.launch.py",
+                    ]
+                ),
+            ]
+        ),
+        launch_arguments={
+            "model": str(Path(aav_bringup) / "aav_yolo_models" / "bullseye.pt"),
+            "input_image_topic": "/siyi_a8/image_raw ",
+        }.items(),
+    )
+
     ld.add_action(drone_mavros_and_camera_boot_launch)
-    ld.add_action(location_logger)
     ld.add_action(manavs_magic_code)
     ld.add_action(object_alignment_controller)
     ld.add_action(topic_converter)
+    ld.add_action(yolo)
 
     return ld
